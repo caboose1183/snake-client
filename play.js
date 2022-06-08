51578578208eea8117b9b94184a6a5f98d183213
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-08 18:03:26 +0000

    added input.js, moved the 2 functions from play to input, added appropriate exports and require lines

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
-const connect = require ('./client')
+const {connect, setupInput} = require ('./client', './input');
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function () {
-  stdin.on ('data', (key) => {
-    if (key === '\u0003') {
-      proces.exit()
-    }
-  })
-};
 
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
