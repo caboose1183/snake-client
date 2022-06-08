947ba8c3ea6165b7f2247a4acc474a4871e00c48
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-08 17:43:58 +0000

    tested moving commands, with set interval and set timeout

--- client.js
+++ client.js
@@ -16,6 +16,12 @@ const connect = function () {
   conn.on ('connect', () => {
     console.log ('Successfully connected to game server')
     conn.write ('Name: SID')
+
+    // setInterval (() => {
+    //   conn.write ('Move: up')
+    // }, 50)
+
+
   })
 
 
